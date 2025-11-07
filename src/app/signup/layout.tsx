2500ef9e0ed1b8f38170977127a739a73688ba51
Eve435-IV <SnowOut435@gmail.com>
2025-11-07 17:11:12 +0700

    07/11/25

--- src/app/signup/layout.tsx
+++ src/app/signup/layout.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import styles from "./authform.module.css";
+import styles from "../profile/profile.module.css";
 
 type AuthFormProps = {
   children?: React.ReactNode; // make optional
