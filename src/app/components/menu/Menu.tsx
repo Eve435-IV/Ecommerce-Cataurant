efc747ded24e9ff9759c227117873f1790570d66
Eve435-IV <SnowOut435@gmail.com>
2025-11-14 13:48:48 +0700

    14/11/25

--- src/app/components/menu/Menu.tsx
+++ src/app/components/menu/Menu.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import styles from "./menu.module.css";
 import { PawPrint } from "lucide-react";
 
 function Menu() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className={styles.mainContianer}>
-      <PawPrint className={styles.icon} />
+    <div className={styles.wrapper}>
+      {/* Button */}
+      <div className={styles.mainContianer} onClick={() => setOpen(!open)}>
+        <PawPrint className={`${styles.icon} ${open ? styles.rotate : ""}`} />
+      </div>
+
+      {/* Pop-out menu */}
+      {open && (
+        <div className={styles.popMenu}>
+          <a href="/home">Home</a>
+          <a href="/menu">Menu</a>
+          <a href="/orders">Orders</a>
+        </div>
+      )}
     </div>
   );
 }
